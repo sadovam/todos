ad5da4377a73fb6d2306927c014b784651e3284a
Andrii <sadovam@gmail.com>
2022-06-10 21:43:08 +0300

    Remove GetItemsByListUid because of it's useless

--- repository/fake_repository.go
+++ repository/fake_repository.go
@@ -13,9 +13,18 @@ func MakeFakeData(totalLists, itemsPerList int) []models.TodoList {
 		todos := make([]models.TodoItem, itemsPerList, itemsPerList)
 		for j := 0; j < itemsPerList; j++ {
 			uid := i*itemsPerList + j
-			todos[j] = models.TodoItem{Uid: uid, ListUid: i, Title: fmt.Sprintf("<=Item %d List %d=>", uid, i), IsDone: uid%3 < 2}
+			todos[j] = models.TodoItem{
+				Uid:     uid,
+				ListUid: i,
+				Title:   fmt.Sprintf("<=Item %d List %d=>", uid, i),
+				IsDone:  uid%3 < 2,
+			}
 		}
-		todoLists[i] = models.TodoList{Uid: i, Title: fmt.Sprintf("<<<List %d>>>", i), Todos: todos}
+		todoLists[i] = models.TodoList{
+			Uid:   i,
+			Title: fmt.Sprintf("<<<List %d>>>", i),
+			Todos: todos,
+		}
 	}
 	return todoLists
 }
@@ -36,14 +45,5 @@ func (repo TodosFake) GetItemByUid(uid int) (*models.TodoItem, error) {
 			}
 		}
 	}
-	return nil, errors.New(fmt.Sprintf("Can't find todo item with id: %d", uid))
-}
-
-func (repo TodosFake) GetItemsByListUid(listUid int) ([]models.TodoItem, error) {
-	for _, list := range repo.data {
-		if list.Uid == listUid {
-			return list.Todos, nil
-		}
-	}
-	return nil, errors.New(fmt.Sprintf("Can't find todo items with list uid: %d", listUid))
+	return nil, errors.New(fmt.Sprintf("TodoItem with uid: %d doesn't exist", uid))
 }
--- repository/fake_repository_test.go
+++ repository/fake_repository_test.go
@@ -5,84 +5,41 @@ import (
 	"testing"
 )
 
-const (
-	totalLists   = 5
-	itemsPerList = 7
-
-	itemUid           = 3
-	itemUIdOutOfRange = 1333
-
-	listUid           = 3
-	listUidOutOfRange = 13
-	itemPosition      = 3
-	itemUidOnPosition = listUid*itemsPerList + itemPosition
-)
-
 func TestMakeFakeData(t *testing.T) {
-	data := MakeFakeData(totalLists, itemsPerList)
-	if len(data) != totalLists {
+	data := MakeFakeData(3, 5)
+	if len(data) != 3 {
 		t.Fatalf("Generating fake data error; may create %d lists, but creating %d",
-			totalLists, len(data))
+			3, len(data))
 	}
-	if len(data[0].Todos) != itemsPerList {
+	if len(data[0].Todos) != 5 {
 		t.Fatalf("Generating fake data error; may create %d todos per list, but creating %d",
-			itemsPerList, len(data))
+			5, len(data))
 	}
 }
 
 func TestGetItemByUid(t *testing.T) {
-	data := MakeFakeData(totalLists, itemsPerList)
+	data := MakeFakeData(5, 7)
 	repo := NewTodosFakeRepository(data)
-	res, err := repo.GetItemByUid(itemUid)
+	res, err := repo.GetItemByUid(23)
 
 	if err != nil {
 		t.Fatalf("GetTestItemByUid error: %v", err.Error())
 	}
 
-	if res.Uid != itemUid {
-		t.Fatalf("GetTestItemByUid error: want uid = %d, got uid = %d", itemUid, res.Uid)
+	if res.Uid != 23 {
+		t.Fatalf("GetTestItemByUid error: want uid = %d, got uid = %d", 23, res.Uid)
 	}
 }
 
 func TestGetItemByUidError(t *testing.T) {
-	data := MakeFakeData(totalLists, itemsPerList)
+	data := MakeFakeData(2, 4)
 	repo := NewTodosFakeRepository(data)
-	res, err := repo.GetItemByUid(1333)
+	res, err := repo.GetItemByUid(15)
 
 	if err == nil {
 		t.Fatalf("GetTestItemByUid error: want error, got result uid = %d", res.Uid)
 	}
-	want := fmt.Sprintf("Can't find todo item with id: %d", itemUIdOutOfRange)
-	if err.Error() != want {
-		t.Fatalf("GetTestItemByUid error: want %s, got %s", want, err.Error())
-	}
-}
-
-func TestGetItemsByListUid(t *testing.T) {
-	data := MakeFakeData(totalLists, itemsPerList)
-	repo := NewTodosFakeRepository(data)
-	res, err := repo.GetItemsByListUid(3)
-
-	if err != nil {
-		t.Fatalf("GetTestItemsByListUid error: %v", err.Error())
-	}
-
-	if res[itemPosition].Uid != itemUidOnPosition || res[itemPosition].ListUid != listUid {
-		t.Fatalf(
-			"GetTestItemsByListUid error: want uid = %d, list uid = %d, got uid = %d, list uid = %d",
-			itemUidOnPosition, listUid, res[itemPosition].Uid, res[itemPosition].ListUid)
-	}
-}
-
-func TestGetItemsByListUidError(t *testing.T) {
-	data := MakeFakeData(totalLists, itemsPerList)
-	repo := NewTodosFakeRepository(data)
-	res, err := repo.GetItemsByListUid(listUidOutOfRange)
-
-	if err == nil {
-		t.Fatalf("GetTestItemByUid error: want error, got result %v", res)
-	}
-	want := fmt.Sprintf("Can't find todo items with list uid: %d", listUidOutOfRange)
+	want := fmt.Sprintf("TodoItem with uid: %d doesn't exist", 15)
 	if err.Error() != want {
 		t.Fatalf("GetTestItemByUid error: want %s, got %s", want, err.Error())
 	}
