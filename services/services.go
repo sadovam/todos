15ca35cfc71b28eda9c079d44a964d8d967cc1f2
Andrii <sadovam@gmail.com>
2022-06-11 23:11:56 +0300

    Add services to implement CRUD for list and item

--- services/services.go
+++ services/services.go
@@ -9,14 +9,33 @@ type TodoItemService struct {
 	repo interfaces.TodosRepository
 }
 
-func NewTodoItemService(repository interfaces.TodosRepository) TodoItemService {
-	return TodoItemService{repo: repository}
+func NewTodoItemService(repository interfaces.TodosRepository) *TodoItemService {
+	return &TodoItemService{repo: repository}
 }
 
-func (serv TodoItemService) GetTodoItemByUId(uid int) (models.TodoItem, error) {
+func (serv *TodoItemService) GetTodoItemByUId(uid int) (*models.TodoItem, error) {
 	return serv.repo.GetItemByUid(uid)
 }
 
-func (serv TodoItemService) GetTodoItemsByListUid(listUid int) ([]models.TodoItem, error) {
-	return serv.repo.GetItemsByListUid(listUid)
+func (serv *TodoItemService) GetTodoListByUid(uid int) (*models.TodoList, error) {
+	return serv.repo.GetListByUid(uid)
+}
+
+func (serv *TodoItemService) CreateItem(listUid int, title string) (*models.TodoItem, error) {
+	return serv.repo.CreateItem(listUid, title)
+}
+func (serv *TodoItemService) CreateList(title string) (*models.TodoList, error) {
+	return serv.repo.CreateList(title)
+}
+func (serv *TodoItemService) UpdateItem(itemUid, listUid int, title string, isDone bool) (*models.TodoItem, error) {
+	return serv.repo.UpdateItem(itemUid, listUid, title, isDone)
+}
+func (serv *TodoItemService) UpdateList(listUid int, title string) (*models.TodoList, error) {
+	return serv.repo.UpdateList(listUid, title)
+}
+func (serv *TodoItemService) DeleteItem(uid int) (*models.TodoItem, error) {
+	return serv.repo.DeleteItem(uid)
+}
+func (serv *TodoItemService) DeleteList(listUid int) (*models.TodoList, error) {
+	return serv.repo.DeleteList(listUid)
 }
